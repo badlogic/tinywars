844311585d307b38add625b6123f7d8798d6f741
Mario Zechner <badlogicgames@gmail.com>
2021-08-15 23:10:10 +0200

    Docker clean-up

--- prod.sh
+++ prod.sh
@@ -7,7 +7,6 @@ git pull
 npm install
 rm -rf build
 tsc -p tsconfig.server.json
-tsc --noEmit -p tsconfig.client.json
 ./node_modules/.bin/esbuild client/tinywars.ts --bundle --minify --sourcemap --outfile=public/tinywars.js
 NODE_DEV=production node build/server.js
 done
